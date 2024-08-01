3b17b396f0067b9128baff7485a12169b439d5ae
meet-tola <wasiusikiru7@gmail.com>
2024-08-01 19:43:04 +0100

    Post controller added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from "cors";
 import authRoute from './routes/auth.route.js';
-import listingRoute from './routes/listing.route.js';
+import postRoute from './routes/post.route.js';
 import userRoute from './routes/user.route.js';
 import dotenv from 'dotenv';
 
@@ -16,7 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/api/listings', listingRoute);
+app.use('/api/posts', postRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute)
 const PORT = process.env.PORT || 8800;
