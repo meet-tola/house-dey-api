fe73eb68d43302cf0d9fd6ee9028536137efd083
meet-tola <wasiusikiru7@gmail.com>
2024-08-31 13:59:08 +0100

    add hosted api to cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://house-dey.vercel.app" || "http://localhost:3000",
     credentials: true,
   })
 );
