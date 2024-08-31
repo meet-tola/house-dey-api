e6f817a708308979cf0482ad47640716bdd8c64f
meet-tola <wasiusikiru7@gmail.com>
2024-08-31 17:24:41 +0100

    change token cookieOption

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,7 +3,10 @@ import jwt from "jsonwebtoken";
 import cookie from "cookie";
 import { v4 as uuidv4 } from "uuid";
 import prisma from "../lib/prisma.js";
-import { sendVerificationEmail, sendResetPasswordEmail  } from "../email/emailService.js";
+import {
+  sendVerificationEmail,
+  sendResetPasswordEmail,
+} from "../email/emailService.js";
 
 export const register = async (req, res) => {
   const { username, email, password, role } = req.body;
@@ -11,7 +14,7 @@ export const register = async (req, res) => {
   try {
     // Hash Password
     const hashedPassword = await bcrypt.hash(password, 10);
-    const verificationToken = uuidv4(); 
+    const verificationToken = uuidv4();
 
     const newUser = await prisma.user.create({
       data: {
@@ -27,7 +30,11 @@ export const register = async (req, res) => {
     // Send verification email
     await sendVerificationEmail(newUser.email, verificationToken);
 
-    res.status(201).send("User registered successfully. Please check your email to verify your account.");
+    res
+      .status(201)
+      .send(
+        "User registered successfully. Please check your email to verify your account."
+      );
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).send("Error registering user");
@@ -41,9 +48,11 @@ export const verifyEmail = async (req, res) => {
     const user = await prisma.user.findFirst({
       where: { verificationToken: token },
     });
-    
+
     if (!user) {
-      return res.status(400).json({ message: "Invalid or expired verification link." });
+      return res
+        .status(400)
+        .json({ message: "Invalid or expired verification link." });
     }
 
     await prisma.user.update({
@@ -61,8 +70,6 @@ export const verifyEmail = async (req, res) => {
   }
 };
 
-
-
 export const login = async (req, res) => {
   const { username, password } = req.body;
 
@@ -72,32 +79,27 @@ export const login = async (req, res) => {
     });
 
     if (!user) return res.status(401).json({ message: "Invalid Credentials" });
-    if (!user.verified) return res.status(401).json({ message: "Please verify your email before logging in." });
+    if (!user.verified)
+      return res
+        .status(401)
+        .json({ message: "Please verify your email before logging in." });
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) return res.status(401).json({ message: "Incorrect Password" });
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Incorrect Password" });
 
-    const age = 1000 * 60 * 60 * 24 * 7; 
-    const token = jwt.sign(
-      { id: user.id },
-      process.env.JWT_SECRET_KEY,
-      { expiresIn: age }
-    );
+    const age = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: age,
+    });
 
-    const cookieOptions = {
-      httpOnly: true,
-      maxAge: age,
-      sameSite: 'None',
-      secure: process.env.NODE_ENV === 'production',
-    };
-
-    if (process.env.NODE_ENV === 'production') {
-      cookieOptions.domain = '.house-dey.vercel.app';
-    }
-
-    // Set the cookie and respond with success
     res
-      .cookie("token", token, cookieOptions)
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+        sameSite: "Lax", 
+        secure: process.env.NODE_ENV === "production",
+      })
       .status(200)
       .json({ message: "Login Successful", user, token });
   } catch (err) {
@@ -106,22 +108,19 @@ export const login = async (req, res) => {
   }
 };
 
-
-
-
 export const logout = (req, res) => {
   res.clearCookie("token").status(200).json({ message: "Logout successful" });
 };
 
 export const checkAuth = async (req, res) => {
-  const cookies = cookie.parse(req.headers.cookie || '');
+  const cookies = cookie.parse(req.headers.cookie || "");
   const token = cookies.token;
   console.log(token);
-  
+
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-  
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     const user = await prisma.user.findUnique({ where: { id: decoded.id } });
@@ -132,7 +131,7 @@ export const checkAuth = async (req, res) => {
 
     res.status(200).json({ user });
   } catch (error) {
-    console.error('Failed to check auth:', error);
+    console.error("Failed to check auth:", error);
     res.status(401).json({ message: "Unauthorized" });
   }
 };
@@ -162,7 +161,9 @@ export const requestPasswordReset = async (req, res) => {
     // Send password reset email
     await sendResetPasswordEmail(user.email, resetToken);
 
-    res.status(200).json({ message: "Password reset link has been sent to your email." });
+    res
+      .status(200)
+      .json({ message: "Password reset link has been sent to your email." });
   } catch (error) {
     console.error("Error requesting password reset:", error);
     res.status(500).json({ message: "Error requesting password reset" });
@@ -203,4 +204,3 @@ export const resetPassword = async (req, res) => {
     res.status(500).json({ message: "Error resetting password" });
   }
 };
-
