09dcb4647202060c949d951ed8a951fb04d459bd
meet-tola <wasiusikiru7@gmail.com>
2024-10-03 12:55:27 +0100

    add featured Post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -104,6 +104,31 @@ export const getAllPosts = async (req, res) => {
   }
 };
 
+export const getFeaturedPosts = async (req, res) => {
+  try {
+    const featuredPosts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: 5,
+      include: {
+        postDetail: true,
+        user: {
+          select: {
+            username: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+
+    res.status(200).json(featuredPosts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get featured posts" });
+  }
+};
+
 export const getPost = async (req, res) => {
   const id = req.params.id;
   try {
--- routes/post.route.js
+++ routes/post.route.js
@@ -2,13 +2,14 @@ import express from "express";
 import {
   getPosts,
   getAllPosts,
+  getFeaturedPosts,
   getPost,
   getUserPosts,
   addPost,
   updatePost,
   deletePost,
   savePost,
-  getSavedPosts 
+  getSavedPosts,
 } from "../controllers/post.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -16,6 +17,7 @@ const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/all", getAllPosts);
+router.get("/featured", getFeaturedPosts);
 router.get("/saved", verifyToken, getSavedPosts);
 router.get("/:id", getPost);
 router.post("/", verifyToken, addPost);
