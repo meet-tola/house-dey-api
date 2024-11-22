47137dc6ea19c16c318ddf343801bc13c17ce329
meet-tola <wasiusikiru7@gmail.com>
2024-11-22 09:12:40 +0100

    fixing messaging with post Id and reset Password email content

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -2,10 +2,11 @@ import prisma from "../lib/prisma.js";
 
 export const addMessage = async (req, res) => {
   const tokenUserId = req.userId;
-  const { text } = req.body;
+  const { text, postId } = req.body;
   const chatId = req.params.chatId;
 
   try {
+    // Find the chat
     const chat = await prisma.chat.findUnique({
       where: {
         id: chatId,
@@ -17,15 +18,18 @@ export const addMessage = async (req, res) => {
 
     if (!chat) return res.status(404).json({ message: "Chat not found!" });
 
+    // Create the message with optional postId
     const message = await prisma.message.create({
       data: {
         chatId: chatId,
         userId: tokenUserId,
         text: text,
         userIDs: chat.userIDs,
+        postId: postId || null, // Ensure null if postId is not provided
       },
     });
 
+    // Update the chat's lastMessage field
     await prisma.chat.update({
       where: { id: chatId },
       data: { lastMessage: text },
